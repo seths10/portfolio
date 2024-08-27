b91f8c611fe9a6adba9f4a2d1899879d990b0397
Seth Addo <addoseth35@gmail.com>
2024-08-27 13:48:03 +0000

    chore: update projects data

--- lib/data.ts
+++ lib/data.ts
@@ -1,5 +1,26 @@
 export const projects = [
-  { id: 1, title: "Project 1", description: "Description for Project 1" },
-  { id: 2, title: "Project 2", description: "Description for Project 2" },
-  { id: 3, title: "Project 3", description: "Description for Project 3" },
+  {
+    id: 1,
+    title: "Quotes Generator",
+    description: "Generate a random quote with a click of a button.",
+    imageUrl: "/images/projects/quote.png",
+    githubUrl: "https://github.com/seths10/Quotes-Generator",
+    liveUrl: "https://seths10.github.io/Quotes-Generator",
+  },
+  {
+    id: 2,
+    title: "Task Clocked",
+    description: "A different way to track your day using a 24hr clock",
+    imageUrl: "/images/projects/task-clocked.png",
+    githubUrl: "https://github.com/seths10/task-clock",
+    liveUrl: "https://task-clocked.vercel.app",
+  },
+  {
+    id: 3,
+    title: "Personal Portfolio",
+    description: "My personal portfolio website",
+    imageUrl: "/images/projects/portfolio.png",
+    githubUrl: "https://github.com/seths10/portfolio",
+    liveUrl: "https://ethr.vercel.app",
+  },
 ];
