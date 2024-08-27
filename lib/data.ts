2bac3f9267a7935a4f9a61753a88d84877edae43
Seth Addo <addoseth35@gmail.com>
2024-08-27 08:56:08 +0000

    chore: move data to separate file

---
+++ lib/data.ts
@@ -0,0 +1,5 @@
+export const projects = [
+  { id: 1, title: "Project 1", description: "Description for Project 1" },
+  { id: 2, title: "Project 2", description: "Description for Project 2" },
+  { id: 3, title: "Project 3", description: "Description for Project 3" },
+];
